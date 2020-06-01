b3a7138b265d3a5ac213ed5ccbe056e7390719ff
Abhi Sharma <aus2101@github.com>
2020-05-31 23:04:35 -0700

    working with py3

--- fdfiles/face_detector.py
+++ fdfiles/face_detector.py
@@ -3,23 +3,36 @@ import numpy as np
 import cv2
 import time
 import requests
+import random
+import os
 
-# 1 should correspond to /dev/video1 , your USB camera. The 0 is reserved for the TX2 onboard camera
-cap = cv2.VideoCapture(1)
+# create temp directory for images
+path = os.getcwd() + "/img"
+try:
+    os.makedirs(path, exist_ok = True)
+except e:
+    print("Could not create image directory")
+    raise e
+
+# download the xml file for the model
 xml_file_url = "https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_default.xml"
 xml_file = "haarcascade_frontalface_default.xml"
-
 r = requests.get(xml_file_url)
 with open(xml_file, 'wb') as f:
     f.write(r.content)
 
+# set up classifier
 face_cascade = cv2.CascadeClassifier(xml_file)
+
+# 1 should correspond to /dev/video1 , your USB camera. The 0 is reserved for the TX2 onboard camera
+cap = cv2.VideoCapture(1)
 
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
 
-    if frame is None:
+    # if frame not captured, sleep 5 seconds
+    if ret is False:
         time.sleep(5)
         continue
 
@@ -27,8 +40,12 @@ while(True):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     for f in faces:
-        print("found a face")
-        continue
+        # bad design, needs to be revisited
+        img_name = "{0}/testimage.jpg".format(path)
+        cv2.imwrite(img_name, frame)
+        image = cv2.imread(img_name)
+        # finally send the image via mqtt
+
 
 # When everything done, release the capture
 cap.release()
