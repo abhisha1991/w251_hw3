b76493fcf97926d5d3ba35e66e8baba05c3ccf1c
root <root@hw3host1.UC-Berkeley.cloud>
2020-06-05 08:05:37 +0000

    added changes to write file to obj storage

--- imgprocfiles/img_processor.py
+++ imgprocfiles/img_processor.py
@@ -1,6 +1,7 @@
 import paho.mqtt.client as mqtt
 import time
 import socket
+import uuid
 
 def on_connect(client, userdata, flags, rc):
     print("Connected to broker with result code :", str(rc))
@@ -16,7 +17,18 @@ def on_message(client, userdata, message):
 
 def send_to_storage(message):
     print("sending message to object storage")
-    # code for sending message
+    # For this, we just store the image in the /data folder in the container. Why?
+    # Because we mounted /data container folder with the /data host machine folder,
+    # the saved files show up in /data in the host machine.
+    # Further, because we connected /data on the host machine to ibm s3
+    # any changes in /data on the host machine show up in ibm s3
+    # Mounting command: s3fs abhihw3bucket $HOME/data -o url=https://s3.us-east.cloud-object-storage.appdomain.cloud -o passwd_file=$HOME/.cos_creds
+    # Container start command: docker run -dit -v ~/data:/data --name test imgproc1
+    file_name = "/data/" + str(uuid.uuid4())
+    f = open(file_name, "w")
+    f.write(message.payload)
+    print("Wrote successfully to file ", file_name)
+    f.close()
 
 hostname = socket.gethostname()
 
@@ -27,7 +39,7 @@ client.on_connect = on_connect
 client.on_message = on_message
 
 print("connecting to broker")
-client.connect("mqtt.eclipse.org", 1883, 60)
+client.connect("127.0.0.1", 1883, 60)
 
 print("Starting a loop on the subscriber")
 # start background daemon to loop forever and listen for that topic
